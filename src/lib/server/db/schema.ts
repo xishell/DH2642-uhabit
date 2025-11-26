f15c8dc60f78eb033f045b2e7a61e59de364b49e
XiShell <26227374+xishell@users.noreply.github.com>
2025-11-26 18:01:10 +0100

    Ran bun run format
    formatted the codebase to make sure the format starts off correct

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,8 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
 	age: integer('age')
 });

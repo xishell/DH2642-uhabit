0dded0194687171c985bdc148b143b80c62de263
Jintong Jiang <jintongj@kth.se>
2025-11-27 12:12:23 +0100

    planning overview page initial commit

---
+++ src/routes/planning/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ToggleBar from './components/ToggleBar.svelte';
+	import HabitCard from './components/HabitCard.svelte';
+	import Btn from '$lib/components/Btn.svelte';
+	let habitList = ['Drink Water', 'Read Book', 'Play with cat', 'Meditation', 'Walk in the woods'];
+</script>
+
+<div class="planning-view flex flex-col items-center p-7 max-w-[800px] m-auto">
+	<h3 class="text-2xl self-start">Plan UHabit</h3>
+	<div
+		class="motivation-card w-full h-40 text-[20px] text-center flex justify-center items-center px-[30px] mt-[45px] mb-[30px] bg-gray-200 rounded-[10px]"
+	>
+		“Let your days echo with the steps you choose to take.”
+	</div>
+	<ToggleBar />
+	<div class="habit-list w-full grid grid-cols-1 sm:grid-cols-2 justify-between gap-7 mt-6">
+		{#each habitList as habit}
+			<HabitCard title={habit} />
+		{/each}
+	</div>
+	<div class="new-btn-ctn absolute bottom-[40px] flex justify-between items-center">
+		<Btn content="New" />
+		<div class=" flex flex-col justify-between">
+			<Btn content="Progressive" />
+			<Btn content="Single-Step" />
+		</div>
+	</div>
+</div>
---
+++ src/routes/planning/components/ToggleBar.svelte
@@ -0,0 +1,30 @@
+<script>
+	let habitType = 0; //0 for progressive habit, 1 for single-step habit
+	let isPActive = true;
+	let isSActive = false;
+	const activateP = () => {
+		isPActive = true;
+		isSActive = false;
+		habitType = 0;
+	};
+
+	const activateS = () => {
+		isPActive = false;
+		isSActive = true;
+		habitType = 1;
+	};
+</script>
+
+<div class="text-[14px] p-[5px] rounded-[40px] bg-gray-100 flex justify-between gap-2">
+	<button
+		class="py-1 px-3 rounded-[40px] hover:bg-gray-300 transition-[1s] cursor-pointer"
+		class:bg-gray-300={isPActive}
+		on:click={activateP}
+		>Progressive
+	</button>
+	<button
+		class="py-1 px-3 rounded-[40px] hover:bg-gray-300 transition-[1s] cursor-pointer"
+		class:bg-gray-300={isSActive}
+		on:click={activateS}>Single-Step</button
+	>
+</div>

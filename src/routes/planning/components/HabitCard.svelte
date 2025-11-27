5a10cb0ee5a5c8985c648c14634337027a6a74e3
Jintong Jiang <jintongj@kth.se>
2025-11-27 12:12:23 +0100

    planning overview page initial commit

---
+++ src/routes/planning/components/HabitCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title: string = 'Drink Water';
+</script>
+
+<div
+	class=" w-full h-[64px] rounded-[10px] bg-gray-100 flex justify-between items-center px-[20px]"
+>
+	<span>{title}</span>
+	<button
+		class="bg-gray-300 rounded-[10px] py-2 px-6 hover:bg-gray-200 transition-all duration-300 cursor-pointer"
+		>Edit</button
+	>
+</div>

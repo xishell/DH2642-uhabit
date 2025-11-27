604f22273b07ad8110b914b60577120cc4306d1b
Jintong Jiang <jintongj@kth.se>
2025-11-27 12:12:23 +0100

    planning overview page initial commit

---
+++ src/lib/components/Btn.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let content = 'button';
+	// export let variant = 'primary';
+	// const variants = {
+	// 	primary: '',
+	// 	secondary: 'bg-gray-200 text-gray-800',
+	// 	danger: 'bg-red-500 text-white'
+	// };
+</script>
+
+<button
+	class="bg-gray-300 rounded-[50px] py-2 px-6 hover:bg-gray-200 transition-colors duration-300 cursor-pointer"
+	>{content}</button
+>

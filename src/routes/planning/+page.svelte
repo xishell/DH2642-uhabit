a13201072646352fd08b632632adbde501ef46c5
Jintong Jiang <jintongj@kth.se>
2025-11-28 12:03:23 +0100

    changed toggle bar logic , finished the new btn layout

--- src/routes/planning/+page.svelte
+++ src/routes/planning/+page.svelte
@@ -2,27 +2,90 @@
 	import ToggleBar from './components/ToggleBar.svelte';
 	import HabitCard from './components/HabitCard.svelte';
 	import Btn from '$lib/components/Btn.svelte';
-	let habitList = ['Drink Water', 'Read Book', 'Play with cat', 'Meditation', 'Walk in the woods'];
+	const progressiveHabitList = [
+		'Drink Water',
+		'Read Book',
+		'Play with cat',
+		'take 10 pictures',
+		'play computer games'
+	];
+	const singleStepHabitList = [
+		'push up once',
+		'practice dance',
+		'write diary',
+		'Meditation',
+		'Walk in the woods',
+		'wash clothes'
+	];
+	let habitType = 0; //0 for progressive habit, 1 for single-step habit
+	let isNewBtnClicked = false;
+
+	function handleHabitTypeChange(event: CustomEvent<0 | 1>) {
+		habitType = event.detail;
+	}
 </script>
 
 <div class="planning-view flex flex-col items-center p-7 max-w-[800px] m-auto">
+	<!-- page title -->
 	<h3 class="text-2xl self-start">Plan UHabit</h3>
+
+	<!-- motivation-card -->
 	<div
 		class="motivation-card w-full h-40 text-[20px] text-center flex justify-center items-center px-[30px] mt-[45px] mb-[30px] bg-gray-200 rounded-[10px]"
 	>
 		“Let your days echo with the steps you choose to take.”
 	</div>
-	<ToggleBar />
-	<div class="habit-list w-full grid grid-cols-1 sm:grid-cols-2 justify-between gap-7 mt-6">
-		{#each habitList as habit}
-			<HabitCard title={habit} />
-		{/each}
-	</div>
-	<div class="new-btn-ctn absolute bottom-[40px] flex justify-between items-center">
-		<Btn content="New" />
-		<div class=" flex flex-col justify-between">
-			<Btn content="Progressive" />
-			<Btn content="Single-Step" />
+
+	<!-- ToggleBar -->
+	<ToggleBar {habitType} on:change={handleHabitTypeChange} />
+
+	<!-- Habit List -->
+	{#if habitType === 0}
+		<div class="habit-list w-full grid grid-cols-1 sm:grid-cols-2 justify-between gap-7 mt-6">
+			{#each progressiveHabitList as habit}
+				<HabitCard title={habit} />
+			{/each}
 		</div>
+	{:else}
+		<div class="habit-list w-full grid grid-cols-1 sm:grid-cols-2 justify-between gap-7 mt-6">
+			{#each singleStepHabitList as habit}
+				<HabitCard title={habit} />
+			{/each}
+		</div>
+	{/if}
+
+	<!-- add new habit btn -->
+	<div
+		class="new-btn-ctn absolute bottom-[40px] right-[45px] flex gap-1.5 justify-between items-center"
+	>
+		<button
+			class="text-3xl w-[70px] h-[70px] bg-gray-300 rounded-full hover:bg-gray-200 transition-colors duration-300 cursor-pointer"
+			on:click={() => {
+				isNewBtnClicked = !isNewBtnClicked;
+			}}>+</button
+		>
+		{#if isNewBtnClicked}
+			<div class=" flex h-[90px] flex-col justify-between">
+				<button
+					class="text-sm bg-gray-200 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer"
+					>Progressive</button
+				>
+				<button
+					class="text-sm bg-gray-200 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer"
+					>Single-Step</button
+				>
+			</div>
+		{:else}
+			<div class=" flex h-[90px] flex-col justify-between">
+				<button
+					class="opacity-0 text-sm bg-gray-200 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer"
+					>Progressive</button
+				>
+				<button
+					class="opacity-0 text-sm bg-gray-200 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer"
+					>Single-Step</button
+				>
+			</div>
+		{/if}
 	</div>
 </div>
--- src/routes/planning/components/ToggleBar.svelte
+++ src/routes/planning/components/ToggleBar.svelte
@@ -1,30 +1,33 @@
-<script>
-	let habitType = 0; //0 for progressive habit, 1 for single-step habit
-	let isPActive = true;
-	let isSActive = false;
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	export let habitType: 0 | 1;
+	const dispatch = createEventDispatcher();
+
+	// let isPActive = true;
+	// let isSActive = false;
 	const activateP = () => {
-		isPActive = true;
-		isSActive = false;
-		habitType = 0;
+		// isPActive = true;
+		// isSActive = false;
+		dispatch('change', 0);
 	};
 
 	const activateS = () => {
-		isPActive = false;
-		isSActive = true;
-		habitType = 1;
+		// isPActive = false;
+		// isSActive = true;
+		dispatch('change', 1);
 	};
 </script>
 
 <div class="text-[14px] p-[5px] rounded-[40px] bg-gray-100 flex justify-between gap-2">
 	<button
 		class="py-1 px-3 rounded-[40px] hover:bg-gray-300 transition-[1s] cursor-pointer"
-		class:bg-gray-300={isPActive}
+		class:bg-gray-300={habitType == 0}
 		on:click={activateP}
 		>Progressive
 	</button>
 	<button
 		class="py-1 px-3 rounded-[40px] hover:bg-gray-300 transition-[1s] cursor-pointer"
-		class:bg-gray-300={isSActive}
+		class:bg-gray-300={habitType == 1}
 		on:click={activateS}>Single-Step</button
 	>
 </div>

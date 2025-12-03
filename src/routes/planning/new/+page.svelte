e2e8ad0c51a46bc6da5af046e125754d0c148826
Jintong Jiang <jintongj@kth.se>
2025-12-03 11:15:41 +0100

    fixed some issue of pr #52, add lucide icone

--- src/routes/planning/new/+page.svelte
+++ src/routes/planning/new/+page.svelte
@@ -1,11 +1,9 @@
-<script>
-	import { DatePicker, Portal } from '@skeletonlabs/skeleton';
-
-	// let value;
+<script lang="ts">
 </script>
 
 <div class="add-habit-view w-full flex flex-col gap-7 p-7 max-w-[800px] m-auto">
 	<h1 class="hidden sm:block text-center text-xl">New Habit</h1>
+	<!-- form body -->
 	<div
 		class="form-info grid grid-cols-1 sm:grid-cols-2 sm:border sm:border-gray-300 sm:rounded-[10px] sm:py-[50px] sm:px-[50px]"
 	>
@@ -45,6 +43,8 @@
 			/>
 		</div>
 	</div>
+
+	<!-- confirmation btns -->
 	<div class="form-btn flex justify-center gap-6">
 		<button
 			class="text-sm bg-gray-100 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer shadow-md"
@@ -58,26 +58,3 @@
 		</button>
 	</div>
 </div>
-
-<!-- <div class="flex flex-col items-center gap-4">
-	<SegmentedControl {value} onValueChange={(details) => (value = details.value)}>
-		<SegmentedControl.Label>Browse</SegmentedControl.Label>
-		<SegmentedControl.Control>
-			<SegmentedControl.Indicator />
-			<SegmentedControl.Item value="music">
-				<SegmentedControl.ItemText>Music</SegmentedControl.ItemText>
-				<SegmentedControl.ItemHiddenInput />
-			</SegmentedControl.Item>
-			<SegmentedControl.Item value="images">
-				<SegmentedControl.ItemText>Images</SegmentedControl.ItemText>
-				<SegmentedControl.ItemHiddenInput />
-			</SegmentedControl.Item>
-			<SegmentedControl.Item value="videos">
-				<SegmentedControl.ItemText>Videos</SegmentedControl.ItemText>
-				<SegmentedControl.ItemHiddenInput />
-			</SegmentedControl.Item>
-		</SegmentedControl.Control>
-	</SegmentedControl>
-
-	<p><span class="opacity-60">You selected</span> <code class="code">{value}</code></p>
-</div> -->

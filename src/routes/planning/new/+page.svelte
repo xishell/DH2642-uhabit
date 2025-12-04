762065eca7030254f2cde401019a8c2b48f2d019
Jintong Jiang <jintongj@kth.se>
2025-12-04 11:35:30 +0100

    done with the basic form element within planning/new page

--- src/routes/planning/new/+page.svelte
+++ src/routes/planning/new/+page.svelte
@@ -1,58 +1,108 @@
 <script lang="ts">
+	import SelectWeekDay from './components/SelectWeekDay.svelte';
+	import SelectMonthDay from './components/SelectMonthDay.svelte';
+	import SelectOrEdit from './components/SelectOrEdit.svelte';
+	import { fade } from 'svelte/transition';
+
+	const colors = ['#E0E0E0', '#CCCCCC', '#B8B8B8', '#A4A4A4', '#909090', '#7C7C7C', '#686868'];
+	const frequencyArr = ['daily', 'weekly', 'monthly'];
+
+	let selectedColor = colors[2];
+	let selectedFrequency = frequencyArr[0];
+
+	function selectColor(color) {
+		selectedColor = color;
+		console.log(color);
+	}
 </script>
 
-<div class="add-habit-view w-full flex flex-col gap-7 p-7 max-w-[800px] m-auto">
+<div class="add-habit-view w-full flex flex-col gap-12 p-7 max-w-[800px] m-auto">
 	<h1 class="hidden sm:block text-center text-xl">New Habit</h1>
 	<!-- form body -->
 	<div
-		class="form-info grid grid-cols-1 sm:grid-cols-2 sm:border sm:border-gray-300 sm:rounded-[10px] sm:py-[50px] sm:px-[50px]"
+		class="form-info grid grid-cols-1 gap-3 sm:gap-0 sm:grid-cols-2 sm:border sm:border-gray-300 sm:rounded-[10px] sm:py-[50px] sm:px-[50px]"
 	>
-		<div class="basic-info flex flex-col gap-2.5 sm:pr-[40px] sm:border-r-gray-300 sm:border-r">
+		<div class="basic-info flex flex-col gap-3 sm:pr-[40px] sm:border-r-gray-300 sm:border-r">
 			<span>Title</span>
 			<input
 				type="text"
-				class="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+				class=" border border-gray-300 rounded-md px-2 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+				placeholder="e.g. drink water"
 			/>
 
 			<span>Notes</span>
 			<textarea
-				class="w-full rounded-md border border-gray-300 px-3 py-2 text-sm
+				class="w-full rounded-md border border-gray-300 px-2 py-2 text-sm
          focus:outline-none focus:ring-2 focus:ring-blue-500"
 				rows="4"
 				placeholder="write down your notes for this habit..."
 			></textarea>
 
 			<span>Color</span>
-			<div class="color-picking-sec w-full h-16 bg-gray-200 rounded-md"></div>
+			<div class="color-picking-sec w-full h-10 rounded-md flex justify-between items-center">
+				{#each colors as color}
+					<button
+						class="color-dot w-10 h-10 sm:w-9 sm:h-9 rounded-full border-2 p-[0.1rem] bg-clip-content transition-all duration-200"
+						style="background-color: {color};"
+						on:click={() => selectColor(color)}
+						style:border-color={selectedColor === color ? selectedColor : 'transparent'}
+						aria-label={`Select color ${color}`}
+					></button>
+				{/each}
+			</div>
 		</div>
-		<div class="additional-info flex flex-col gap-2.5 sm:pl-[40px]">
+
+		<div class="additional-info flex flex-col gap-3 sm:pl-[40px]">
 			<span>Frequency</span>
-			<input
-				type="text"
-				class="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-			/>
+			<div class="flex flex-col gap-2.5">
+				<div class="w-full h-10 rounded-md flex justify-between items-center">
+					{#each frequencyArr as frequency}
+						<button
+							type="button"
+							class="btn capitalize preset-outlined-surface-500 border-primary-600 transition-colors duration-200 sm:text-sm"
+							class:border-transparent={!(selectedFrequency === frequency)}
+							class:text-primary-700={selectedFrequency === frequency}
+							on:click={() => (selectedFrequency = frequency)}
+						>
+							{frequency}
+						</button>
+					{/each}
+				</div>
+				{#if selectedFrequency === 'weekly'}
+					<SelectWeekDay />
+				{:else if selectedFrequency === 'monthly'}
+					<SelectMonthDay />
+				{:else}{/if}
+			</div>
+
 			<span>Measurement</span>
+			<div class="inputs-ctn flex gap-4">
+				<input
+					type="text"
+					class="border border-gray-300 w-18 h-9 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-400 text-right pr-2"
+					placeholder="100"
+				/>
+				<SelectOrEdit />
+			</div>
+
+			<!-- future development [Goal] -->
+			<!-- <span>Period(optional)</span>
 			<input
 				type="text"
 				class="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-			/>
-			<span>Period(optional)</span>
-			<input
-				type="text"
-				class="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-			/>
+			/> -->
 		</div>
 	</div>
 
 	<!-- confirmation btns -->
-	<div class="form-btn flex justify-center gap-6">
+	<div class="form-btn flex justify-center gap-8">
 		<button
-			class="text-sm bg-gray-100 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer shadow-md"
+			class="text-sm w-24 bg-gray-100 rounded-[50px] py-3 px-6 hover:bg-gray-300 transition-colors duration-300 cursor-pointer shadow-sm"
 		>
 			Cancel
 		</button>
 		<button
-			class="text-sm bg-gray-100 rounded-[50px] py-2 px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer shadow-md"
+			class="text-md w-24 bg-gray-100 rounded-[50px] py-2 px-5 hover:bg-gray-300 transition-colors duration-300 cursor-pointer shadow-sm"
 		>
 			Create
 		</button>
---
+++ src/routes/planning/new/components/SelectWeekDay.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+	let selected = new Set<string>();
+
+	let isDragging = false;
+	let dragMode: 'select' | 'deselect' | null = null;
+
+	function updateSet() {
+		selected = new Set(selected);
+	}
+
+	function startDrag(day: string, e?: TouchEvent | MouseEvent) {
+		e?.preventDefault();
+
+		isDragging = true;
+
+		if (selected.has(day)) {
+			dragMode = 'deselect';
+			selected.delete(day);
+		} else {
+			dragMode = 'select';
+			selected.add(day);
+		}
+		updateSet();
+	}
+
+	function dragOver(day: string) {
+		if (!isDragging || !dragMode) return;
+
+		if (dragMode === 'select') selected.add(day);
+		if (dragMode === 'deselect') selected.delete(day);
+
+		updateSet();
+	}
+
+	function endDrag() {
+		isDragging = false;
+		dragMode = null;
+	}
+
+	function handleTouchMove(e: TouchEvent) {
+		if (!isDragging) return;
+
+		const touch = e.touches[0];
+		const target = document.elementFromPoint(touch.clientX, touch.clientY);
+		const day = target?.getAttribute('data-day');
+
+		if (day) dragOver(day);
+	}
+</script>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div
+	class="grid grid-cols-7 gap-[1px] select-none touch-none rounded-md overflow-clip"
+	on:mouseup={endDrag}
+	on:mouseleave={endDrag}
+	on:touchend={endDrag}
+	on:touchcancel={endDrag}
+	on:touchmove={handleTouchMove}
+>
+	{#each days as day}
+		<div
+			data-day={day}
+			class="
+				 h-10 text-sm sm:text-[13px] sm:h-8 flex items-center justify-center
+				cursor-pointer transition-all duration-150
+				{selected.has(day)
+				? 'bg-secondary-400 text-white border border-secondary-400'
+				: 'bg-primary-50 text-primary-700 hover:bg-gray-200'}
+			"
+			on:mousedown={(e) => startDrag(day, e)}
+			on:touchstart={(e) => startDrag(day, e)}
+			on:mouseenter={() => dragOver(day)}
+		>
+			{day}
+		</div>
+	{/each}
+</div>

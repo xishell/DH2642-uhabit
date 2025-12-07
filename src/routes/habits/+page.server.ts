c77618b306833c39622cc980bd96a443c824e10d
Jintong Jiang <jintongj@kth.se>
2025-12-07 11:49:21 +0100

    commit the rest of files

--- src/routes/habits/+page.server.ts
+++ src/routes/habits/+page.server.ts
@@ -1,15 +1,15 @@
-// import type { PageServerLoad } from './$types';
-// import type { Habit } from '$lib/types/habit';
+import type { PageServerLoad } from './$types';
+import type { Habit } from '$lib/types/habit';
 
-// export const load: PageServerLoad = async ({ fetch }) => {
-// 	const habits = (await fetch('/api/habits').then((r) => r.json())) as Habit[];
+export const load: PageServerLoad = async ({ fetch }) => {
+	const habits = (await fetch('/api/habits').then((r) => r.json())) as Habit[];
 
-// 	const progressiveHabitList = habits.filter((h: any) => h.measurement === 'numeric');
+	const progressiveHabitList = habits.filter((h: any) => h.measurement === 'numeric');
 
-// 	const singleStepHabitList = habits.filter((h: any) => h.measurement === 'boolean');
+	const singleStepHabitList = habits.filter((h: any) => h.measurement === 'boolean');
 
-// 	return {
-// 		progressiveHabitList,
-// 		singleStepHabitList
-// 	};
-// };
+	return {
+		progressiveHabitList,
+		singleStepHabitList
+	};
+};

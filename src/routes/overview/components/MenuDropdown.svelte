e43e1d408af4e07a072fc8c26130f3aaf1f0e16d
XiShell <26227374+xishell@users.noreply.github.com>
2025-12-08 15:07:07 +0100

    chore: code formatting

--- src/routes/overview/components/MenuDropdown.svelte
+++ src/routes/overview/components/MenuDropdown.svelte
@@ -11,10 +11,34 @@
 	>
 	{#if open}
 		<ul class="absolute right-0 mt-2 bg-white shadow rounded-md w-44 z-40">
-			<li><a href="/overview" data-sveltekit-preload-data="hover" class="block px-4 py-2 hover:bg-surface-50">Overview</a></li>
-			<li><a href="/habits/new" data-sveltekit-preload-data="hover" class="block px-4 py-2 hover:bg-surface-50">Create</a></li>
-			<li><a href="/statistics" data-sveltekit-preload-data="hover" class="block px-4 py-2 hover:bg-surface-50">Statistics</a></li>
-			<li><a href="/settings" data-sveltekit-preload-data="hover" class="block px-4 py-2 hover:bg-surface-50">Settings</a></li>
+			<li>
+				<a
+					href="/overview"
+					data-sveltekit-preload-data="hover"
+					class="block px-4 py-2 hover:bg-surface-50">Overview</a
+				>
+			</li>
+			<li>
+				<a
+					href="/habits/new"
+					data-sveltekit-preload-data="hover"
+					class="block px-4 py-2 hover:bg-surface-50">Create</a
+				>
+			</li>
+			<li>
+				<a
+					href="/statistics"
+					data-sveltekit-preload-data="hover"
+					class="block px-4 py-2 hover:bg-surface-50">Statistics</a
+				>
+			</li>
+			<li>
+				<a
+					href="/settings"
+					data-sveltekit-preload-data="hover"
+					class="block px-4 py-2 hover:bg-surface-50">Settings</a
+				>
+			</li>
 		</ul>
 	{/if}
 </div>
